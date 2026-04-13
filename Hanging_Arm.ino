8a08c35da43572b00fc9f572b23faa0861e2486a
Straszy <70981849+straszydto@users.noreply.github.com>
2026-04-13 15:46:41 -0400

    Eye Servo Movement
    Added Eye Movement
    LT - Open Eye
    RT - Close Eye

--- Hanging_Arm.ino
+++ Hanging_Arm.ino
@@ -1,8 +1,16 @@
 #include <Bluepad32.h>
+#include <Servo.h>
 
 //define controller
 ControllerPtr myController = nullptr;
 
+Servo eyeServo;
+
+const int eyePin=18;
+
+int eyeAngle = 90;
+
+const int deadzone=200;
 
 void onConnectedController(ControllerPtr ctl) {
   Serial.println("Controller Connectected!");
@@ -14,11 +22,14 @@ void onDisconnectedController(ControllerPtr ctl) {
   myController = nullptr;
 }
 
-unsigned long lastOutput = 0;
+unsigned long lastOutput = 0, lastMove=0;
 
 void setup() {
   Serial.begin(115200);
   delay(1000);
+
+  eyeServo.attach(eyePin);
+  eyeServo.write(eyeAngle);
 
   BP32.setup(&onConnectedController, &onDisconnectedController);
 
@@ -34,9 +45,14 @@ void loop() {
   }
 
   unsigned long now = millis();
+
+  int rt = myController->throttle();
+  int lt = myController->brake();
+
   if (now-lastOutput >= 2500) {
     lastOutput=now;
-    Serial.printf("Let Stick X: %4d, Y: %4d\nRight Stick X: %4d, Y: %4d\nRT: %4d, LT: %4d\n", 
+    Serial.printf("Battery: %f, Let Stick X: %4d, Y: %4d\nRight Stick X: %4d, Y: %4d\nRT: %4d, LT: %4d\n",
+    myController->battery(),
     myController->axisX(),
     myController->axisY(),
     myController->axisRX(),
@@ -44,6 +60,20 @@ void loop() {
     myController->throttle(),
     myController->brake()
     );
+
+    Serial.printf("Eye Angle: %d", eyeAngle);
   }
 
+  if (now-lastMove >=200) {
+    now=lastMove;
+    if (rt > deadzone && eyeAngle<180) {
+      eyeAngle+=2;
+      eyeServo.write(eyeAngle);
+    } else if (lt > deadzone && eyeAngle>0) {
+      eyeAngle-=2;
+      eyeServo.write(eyeAngle);
+    }
+  }
+  //Eye Movement
+  delay(10);
 }
